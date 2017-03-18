70675f74248b3fbdce1614c32a8bd14dc76d41fe
HimaJyun <HimaJyun@users.noreply.github.com>
2017-03-18 13:34:43 +0900

    typo

---
+++ Zabbigot/src/main/java/jp/jyn/zabbigot/Zabbigot.java
@@ -0,0 +1,56 @@
+package jp.jyn.zabbigot;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Zabbigot extends JavaPlugin {
+
+	private ConfigStruct config;
+
+	private TpsWatcher tps;
+	private ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+	private ScheduledFuture<?> sender;
+
+	@Override
+	public void onEnable() {
+		if (config != null) { // どれかがnullでなければリロード呼び出しなので無効化する
+			onDisable();
+		}
+
+		// nullなら読み込み、違えばリロード
+		config = (config == null ? new ConfigStruct(this) : config.reloadConfig());
+
+		// TPSの記録を開始
+		tps = new TpsWatcher(this);
+		// 送信開始
+		sender = service.scheduleAtFixedRate(new StatusSender(this), config.getPeriod(), config.getPeriod(), TimeUnit.SECONDS);
+	}
+
+	@Override
+	public void onDisable() {
+		// イベント無効化
+		HandlerList.unregisterAll(this);
+
+		// TPS記録を止める
+		tps.cancel();
+
+		// 送信を止める
+		if (sender != null) {
+			sender.cancel(false);
+		}
+		service.shutdown();
+	}
+
+	public ConfigStruct getConfigStruct() {
+		return config;
+	}
+
+	public TpsWatcher getTpsWatchar() {
+		return tps;
+	}
+}
--- Zabbigot/src/main/java/jp/jyn/zabbigot/Zabbitgot.java
+++
@@ -1,7 +0,0 @@
-package jp.jyn.zabbigot;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class Zabbitgot extends JavaPlugin {
-
-}

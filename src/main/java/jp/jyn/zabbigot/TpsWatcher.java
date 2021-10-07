d0f5f4aa1a15211991d94f222a346975b9e002ba
HimaJyun <HimaJyun@users.noreply.github.com>
2021-10-07 20:03:18 +0900

    TPS count by nanoTime()

--- src/main/java/jp/jyn/zabbigot/TpsWatcher.java
+++ src/main/java/jp/jyn/zabbigot/TpsWatcher.java
@@ -3,9 +3,10 @@ package jp.jyn.zabbigot;
 public class TpsWatcher implements Runnable {
     // 2^n
     private final static int MAX_SAMPLING_SIZE = 256;
+    private final static double TIMER_UNIT =  1_000_000_000.0D;
 
-    private int tickCount = 0;
-    private long[] ticks = new long[MAX_SAMPLING_SIZE];
+    private int tickCount = -1;
+    private final long[] ticks = new long[MAX_SAMPLING_SIZE];
 
     public double getTPS() {
         return getTPS(100);
@@ -16,14 +17,14 @@ public class TpsWatcher implements Runnable {
             return 20.0D;
         }
         int target = (tickCount - 1 - tick) & (MAX_SAMPLING_SIZE - 1);
-        long elapsed = System.currentTimeMillis() - ticks[target];
+        long elapsed = System.nanoTime() - ticks[target];
 
-        return tick / (elapsed / 1000.0D);
+        return tick / (elapsed / TIMER_UNIT);
     }
 
     public void run() {
         // &(MAX_SAMPLING_SIZE-1) == %MAX_SAMPLING_SIZE
-        ticks[(tickCount++ & (MAX_SAMPLING_SIZE - 1))] = System.currentTimeMillis();
+        ticks[(++tickCount & (MAX_SAMPLING_SIZE - 1))] = System.nanoTime();
     }
 
 }
